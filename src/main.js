b112a5ad52662836c6e36a5b78f1fd473204e0b1
Alex <alex830216@gmail.com>
2023-03-08 18:26:17 +0800

    add veevalidate

--- src/main.js
+++ src/main.js
@@ -3,15 +3,39 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import {
+  Form, Field, ErrorMessage, defineRule, configure
+} from 'vee-validate'
+import AllRules from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
 import './assets/all.scss'
 
+// 載入規則
+Object.keys(AllRules).forEach((rule) => {
+  defineRule(rule, AllRules[rule])
+})
+configure({
+  // 載入繁體中文語系
+  generateMessage: localize({ zh_TW: zhTW }),
+  // 當輸入任何內容直接進行驗證
+  validateOnInput: true
+})
+// 設定預設語系
+setLocale('zh_TW')
+
 const app = createApp(App)
 
 app.use(VueAxios, axios)
-
 app.use(router)
+
+app.component('VField', Field)
+app.component('VForm', Form)
+app.component('ErrorMessage', ErrorMessage)
+
 app.mount('#app')
--- src/views/front/CartView.vue
+++ src/views/front/CartView.vue
@@ -1,62 +1,135 @@
 <template>
   <div>這是購物車頁面</div>
-  <table class="table align-middle">
-    <thead>
-      <tr>
-        <th></th>
-        <th>品名</th>
-        <th style="width: 150px">數量/單位</th>
-        <th>單價</th>
-      </tr>
-    </thead>
-    <tbody>
-      <template v-if="cart.carts">
-        <tr v-for="item in cart.carts" :key="item.id">
-          <td>
-            <!-- 刪除時不能重複刪除 -->
-            <button type="button" class="btn btn-outline-danger btn-sm"
-              :disabled="item.id === loadingItem"
-              @click="deleteCart(item)">
-              <i class="fas fa-spinner fa-pulse"
-                v-if="item.id === loadingItem"></i>
-              x
-            </button>
-          </td>
-          <td>
-            {{ item.product.title }}
-            <div class="text-success">
-              已套用優惠券
-            </div>
-          </td>
-          <td>
-            <div class="input-group input-group-sm">
-              <div class="input-group mb-3">
-                <!-- 更新時不能更改數量 -->
-                <select name="" id="" class="form-control" v-model="item.qty"
-                  :disabled="item.id === loadingItem"
-                  @change="updateCart(item)">
-                  <option :value="i" v-for="i in 20" :key="`${i}165146`">{{ i }}</option>
-                </select>
-              </div>
-            </div>
-          </td>
-          <td class="text-end">
-            {{ item.product.price }}
-          </td>
+  <div class="container">
+    <table class="table align-middle">
+      <thead>
+        <tr>
+          <th></th>
+          <th>品名</th>
+          <th style="width: 150px">數量/單位</th>
+          <th>單價</th>
         </tr>
-      </template>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colspan="3" class="text-end">總計</td>
-        <td class="text-end">{{ cart.total }}</td>
-      </tr>
-      <tr>
-        <td colspan="3" class="text-end text-success">折扣價</td>
-        <td class="text-end text-success">{{ cart.final_total }}</td>
-      </tr>
-    </tfoot>
-  </table>
+      </thead>
+      <tbody>
+        <template v-if="cart.carts">
+          <tr v-for="item in cart.carts" :key="item.id">
+            <td>
+              <!-- 刪除時不能重複刪除 -->
+              <button type="button" class="btn btn-outline-danger btn-sm"
+                :disabled="item.id === loadingItem"
+                @click="deleteCart(item)">
+                <i class="fas fa-spinner fa-pulse"
+                  v-if="item.id === loadingItem"></i>
+                x
+              </button>
+            </td>
+            <td>
+              {{ item.product.title }}
+              <div class="text-success">
+                已套用優惠券
+              </div>
+            </td>
+            <td>
+              <div class="input-group input-group-sm">
+                <div class="input-group mb-3">
+                  <!-- 更新時不能更改數量 -->
+                  <select name="" id="" class="form-control" v-model="item.qty"
+                    :disabled="item.id === loadingItem"
+                    @change="updateCart(item)">
+                    <option :value="i" v-for="i in 20" :key="`${i}165146`">{{ i }}</option>
+                  </select>
+                </div>
+              </div>
+            </td>
+            <td class="text-end">
+              {{ item.product.price }}
+            </td>
+          </tr>
+        </template>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="3" class="text-end">總計</td>
+          <td class="text-end">{{ cart.total }}</td>
+        </tr>
+        <tr>
+          <td colspan="3" class="text-end text-success">折扣價</td>
+          <td class="text-end text-success">{{ cart.final_total }}</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+  <div class="my-5 row justify-content-center">
+    <v-form ref="form" class="col-md-6" v-slot="{ errors }" @submit="createOrder">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <v-field
+          id="email"
+          name="email"
+          type="email"
+          class="form-control"
+          :class="{ 'is-invalid': errors['email'] }"
+          placeholder="請輸入 Email"
+          rules="required|email"
+          v-model="form.user.email"
+        ></v-field>
+        <error-message name="email" class="invalid-feedback"></error-message>
+      </div>
+
+      <div class="mb-3">
+        <label for="name" class="form-label">收件人姓名</label>
+        <v-field
+          id="name"
+          name="姓名"
+          type="text"
+          class="form-control"
+          :class="{ 'is-invalid': errors['姓名'] }"
+          placeholder="請輸入姓名"
+          rules="required"
+          v-model="form.user.name"
+        ></v-field>
+        <error-message name="姓名" class="invalid-feedback"></error-message>
+      </div>
+
+      <div class="mb-3">
+        <label for="tel" class="form-label">收件人電話</label>
+        <v-field
+          id="tel"
+          name="電話"
+          type="tel"
+          class="form-control"
+          :class="{ 'is-invalid': errors['電話'] }"
+          placeholder="請輸入電話"
+          rules="required|min:8|max:10"
+          v-model="form.user.tel"
+        ></v-field>
+        <error-message name="電話" class="invalid-feedback"></error-message>
+      </div>
+
+      <div class="mb-3">
+        <label for="address" class="form-label">收件人地址</label>
+        <v-field
+          id="address"
+          name="地址"
+          type="text"
+          class="form-control"
+          :class="{ 'is-invalid': errors['地址'] }"
+          placeholder="請輸入地址"
+          rules="required"
+          v-model="form.user.address"
+        ></v-field>
+        <error-message name="地址" class="invalid-feedback"></error-message>
+      </div>
+
+      <div class="mb-3">
+        <label for="message" class="form-label">留言</label>
+        <textarea id="message" class="form-control" cols="30" rows="10" v-model="form.message"></textarea>
+      </div>
+      <div class="text-end">
+        <button type="submit" class="btn btn-danger">送出訂單</button>
+      </div>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -121,6 +194,9 @@ export default {
         this.form.message = ''
         this.getCarts()
       })
+        .catch(err => {
+          alert(err.response.data.message)
+        })
     }
   },
   mounted () {
